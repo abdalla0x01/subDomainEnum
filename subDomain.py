140179ab326e6865b0b6e7624d6b98981f995490
abdalla0x01 <62313618+abdalla0x01@users.noreply.github.com>
2020-04-27 11:57:41 -0400

    Update subDomain.py

--- subDomain.py
+++ subDomain.py
@@ -1,15 +1,18 @@
+
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+import random
 import os
 import pyfiglet
 import argparse
 import sys
 import time 
-import random
 import string
 import paramiko
 from datetime import datetime
+from random import randint
+from asciimatics.screen import Screen
+
 
 #positional args
 
@@ -45,6 +48,7 @@ def randomString(stringLength=10):
     return ''.join(random.choice(letters) for i in range(stringLength))
 rando1 = randomString()
 rando2 = randomString()
+rando3 = randomString()
 def slowprint(s):  # slow print
     for c in s + '\n':
         sys.stdout.write(c)
@@ -60,6 +64,8 @@ subl = pyfiglet.figlet_format('sublist3r') # art
 censys = pyfiglet.figlet_format('censys') # art
 crtsh_enum_art = pyfiglet.figlet_format('crtsh_enum_psql') # art
 bruteart = pyfiglet.figlet_format('brute') # art 
+cert = pyfiglet.figlet_format('certsh') # art 
+sec = pyfiglet.figlet_format('securitytrails') # art 
 print(ascii_banner)
 
 slowprint('----------------------')
@@ -69,7 +75,7 @@ slowprint(time_file + '  -')
 shr = '---------------------------------------------------'
 res = '\n Result:'
 print(shr)
-check = ('.ac','.ad','.ae','land','.aero','.ag','.al','.am','.asia','.at','.az','.b','.be','.bg','.biz','.bo','.br','.bz','.ca','.cc','.cf','.ch','.cl','.cm','.cn','.co','.com','.cz','.de','.dk','.dz','.ec','.edu','.es','.eu','.fi','.fm','.fr','.ga','.gd','.gen','.gi','.gl','.go','.gob','.gov','.gr','.gs','.hk','.hr','.hu','.ie','.im','.in','.info','.int','.io','.iq','.ir','.is','.it','.jp','.kr','.kz','.la','.li','.lk','.lt','.lu','.lv','.ly','.md','.me','.mil','.mobi','.mp','.ms','.msk','.mus','.mx','.my','.ne','.net','.nhs','.nl','.no','.or','.org','.pe','.ph','.pl','.pn','.pt','.re','.ro','.rs','.ru','.sc','.se','.sg','.sh','.sk','.sm','.sn','.st','.su','.sx','.tk','.tl','.to','.tt','.tv','.ua','.uk','.us','.vc','.vn','.ws','.xxx')
+check = ('.ac','.ad','.ae','land','.aero','.ag','.al','.am','.asia','.games','.il','.at','.az','.b','.be','.bg','.biz','.bo','.br','.bz','.ca','.cc','.cf','.ch','.cl','.cm','.cn','.co','.com','.cz','.de','.dk','.dz','.ec','.edu','.es','.eu','.fi','.fm','.fr','.ga','.gd','.gen','.gi','.gl','.go','.gob','.gov','.gr','.gs','.hk','.hr','.hu','.ie','.im','.in','.info','.int','.io','.iq','.ir','.is','.it','.jp','.kr','.kz','.la','.li','.lk','.lt','.lu','.lv','.ly','.md','.me','.mil','.mobi','.mp','.ms','.msk','.mus','.mx','.my','.ne','.net','.nhs','.nl','.no','.or','.org','.pe','.ph','.pl','.pn','.pt','.re','.ro','.rs','.ru','.sc','.se','.sg','.sh','.sk','.sm','.sn','.st','.su','.sx','.tk','.tl','.to','.tt','.tv','.ua','.uk','.us','.vc','.vn','.ws','.xxx')
 domain = str(args.dom)
 grep = " | grep '" + domain + "'"
 
@@ -80,25 +86,34 @@ else:
 
 save = 'result/' + file
 f=open(save,'a+')
+#amass
 amass = 'amass enum -d ' + domain + ' -o ' + 'result/' + file 
+#aquatone
 aquatone = 'aquatone-discover -d ' + domain 
 aqufile = 'cat /home/abdalla/aquatone/'+domain+'/hosts.txt | cut -d "," -f 1 >> result/' + file
+#passhunt
 passhunt = 'python3 tool/Passivehunter/passivehunter.py ' + domain  + ' >> result/' + file
+#subfinder
 subfi = 'subfinder -d ' +  domain + ' -o ' + '.test/' + rando1
 subfifile = 'cat ' + '.test/' + rando1 + ' >> result/' + file
+#securitytrails
+commd  = 'curl --url https://api.securitytrails.com/v1/domain/'+domain+'/subdomains   --header "apikey: OyCXaiWSyGXt7t3WegehSIiYgaRiBDe8" | awk \'BEGIN{FS="\\""} { for (i=2; i<=NF; i++) print $i }\' | sed "s/,//g"  | cut -d ":" -f  1  | sort -u  | sort -u | sed "s/\\// /g" | sed "s/ /\\n/g" | sort -u  > .test/'+rando3+';for i in $(cat .test/'+rando3+');do echo $i"."'+domain+';done >> result/'+domain+''
+#sublist3r
 sublist3r = 'python3 tool/Sublist3r/sublist3r.py -d ' + domain + ' -o .test/' + rando2
-sublistfile = 'cat .test/' + rando2 + ' | awk \'BEGIN{FS="<BR>"} { for (i=2; i<=NF; i++) print $i }\' ' + '>> result/' + file  
-subnum = 'python tool/subnum/script.py ' + domain + ' >> result/' + file
-crtsh_enum_psql = 'python3 tool/the-art-of-subdomain-enumeration/crtsh_enum_psql.py ' + domain + ' >>' + 'result/'+ file
+sublistfile = 'cat .test/' + rando2 + ' | awk \'BEGIN{FS="<BR>"} { for (i=2; i<=NF; i++) print $i }\' ' + '>> result/' + file
+#brute
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('', username='', password='', key_filename='')
+ssh.connect('3.122.237.234', username='bitnami', password='', key_filename='tool/LightsailDefaultKey-eu-central-1.pem')
+print(amass)
 if domain:
     if domain.endswith(check) == False:
         print('ERROR: Pleas Check Domain')
     else:
-        print(ama, shr, res) #amass
+        print(ama, shr, res) #aquatone
         os.system(amass)
+        print('DONE...\n' + '--------------------------------------------------\n'+ sec, shr, res) #aquatone
+        os.system(commd)
         print('DONE...\n' + '--------------------------------------------------\n' + aqu, shr, res) #aquatone
         os.system(aquatone)
         os.system(aqufile)
@@ -111,10 +126,6 @@ if domain:
         print(sublist3r)
         os.system(sublist3r)
         os.system(sublistfile)
-        """
-        print('DONE...\n' + '--------------------------------------------------\n' + censys, shr, res) #subnum
-        os.system(subnum)
-        """
         print('DONE...\n' + '--------------------------------------------------\n' + bruteart, shr, res) #brute
         print('sed -e \'s/$/.'+domain+'/\' SubDomainsEnum/jhaddix_commonspeak.txt > company_profile.txt | python SubDomainsEnum/MassForce.py SubDomainsEnum/company_profile.txt | grep \''+domain+'\' | cut -d " " -f 1')
         stdin, stdout, stderr = ssh.exec_command('sed -e \'s/$/.'+domain+'/\' SubDomainsEnum/jhaddix_commonspeak.txt > SubDomainsEnum/company_profile.txt ; python SubDomainsEnum/MassForce.py SubDomainsEnum/company_profile.txt | grep \''+domain+'\' | cut -d " " -f 1')
